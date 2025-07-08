0b0893b826bc2a53378800301a84ab4495d48196
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-07-07 20:19:54 -0600

    attempt to visualize some indicators of before_1980

--- p4_source.py
+++ p4_source.py
@@ -4,3 +4,27 @@ from lets_plot import *
 
 LetsPlot.setup_html(isolated_frame=True)
 
+df = pd.read_csv('/home/dallinmoak/school/ds250-wrapper/p4/dwellings_ml.csv')
+
+potential_indicators = [
+    'netprice',
+    'livearea',
+    'totunits',
+    'numbdrm',
+    'numbaths',
+    'nocars',
+    'stories',
+]
+
+raw_prospects = df[
+    potential_indicators + ['yrbuilt']
+]
+
+prospects = pd.DataFrame()
+
+prospects['before_1980'] = raw_prospects['yrbuilt'].apply(lambda x: x < 1980)
+
+prospects = prospects.join(raw_prospects[potential_indicators])
+
+before_1980_aggs = prospects.groupby('before_1980').mean().reset_index()
+
